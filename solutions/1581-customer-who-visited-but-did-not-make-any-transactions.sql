a66e8d47a76ea215c7f733681d60ddfb7b9659f4
mohamadhesham00 <mohamadhesham112@gmail.com>
2024-12-14 11:18:59 +0200

    8- customer-who-visited-but-did-not-make-any-transactions

---
+++ solutions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,6 @@
+select customer_id, count(V.visit_id) as count_no_trans
+from Visits as V
+left join Transactions as T
+on V.visit_id = T.visit_id
+where T.transaction_id is null 
+group by customer_id
